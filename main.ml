faca2c9cfbbf11702d81f37bdf0ad9523491d8d9
Oleg <forown@yandex.ru>
2018-07-06 17:16:15 -0400

    +1

--- main.ml
+++ main.ml
@@ -2,21 +2,6 @@ open Core_kernel
 open Format
 open Bap.Std
 
-module type What_I_am_going_to_do = sig
-
-  type t
-  type semantics
-
-  val ieee_singe : semantics
-  (* other semantics here *)
-
-  val of_bits: semantics -> word -> t
-  val of_float: float -> t
-  val add : t -> t -> t
-  val string_of_t : t -> string
-
-end
-
 let enum_bits w =
   let bits = Word.enum_bits w BigEndian in
   let b_len = Seq.length bits in
@@ -34,10 +19,13 @@ let string_of_bits w =
 
 module Sema = struct
 
+  (* precision is a total digits, with an explicit integer bit,
+     as it is in ieee 754 *)
   type t = {
     max_exponent : int;
     precision : int;
     total : int;
+    integer_bit : bool;
   }
 
   let min_exponent t = 1 - t.max_exponent
@@ -45,22 +33,65 @@ module Sema = struct
   let precision t = t.precision
   let total t = t.total
   let bias t = t.max_exponent
+  let integer_bit t = t.integer_bit
 
-  let ieee_single = {
-    max_exponent = 127;
-    precision = 24;
-    total = 32;
-  }
+  let pow2 n = int_of_float (2.0 ** float_of_int n)
 
-  let ieee_double = {
-    max_exponent = 1023;
-    precision = 53;
-    total = 64;
-  }
+  let create ?(integer_bit=false) ~exp_bits total =
+    let max_exponent = pow2 (exp_bits - 1) - 1 in
+    let precision =
+      if integer_bit then total - exp_bits - 1
+      else  total - exp_bits in
+    { max_exponent; precision; total; integer_bit; }
+
+  let ieee_single = create ~exp_bits:8 32
+  let ieee_double = create ~exp_bits:11 64
+  let x87_double_ext = create ~integer_bit:true ~exp_bits:15 80
 
 end
 
 type sema = Sema.t
+
+type cmp =
+  | Unordered
+  | LessThan
+  | GreaterThan
+  | Equal
+
+type cat =
+  | Normal
+  | Subnormal
+  | Nan
+  | Inf
+  | Zero
+
+module Category = struct
+
+  type t = cat
+
+  let is_normal sema e =
+    1 - Sema.bias sema <= e && e <= 2 * Sema.max_exponent sema
+
+  let is_nan sema e fra =
+    e = 2 * Sema.max_exponent sema + 1 && not (Word.is_zero fra)
+
+  let is_inf sema e fra =
+    e = 2 * Sema.max_exponent sema + 1 && Word.is_zero fra
+
+  let is_subnormal e fra = e = 0 && not (Word.is_zero fra)
+  let is_zero e fra = e = 0 && Word.is_zero fra
+
+  (** [create sema biased_exp fraction]  *)
+  let create sema exp fra =
+    let exp = exp - Sema.bias sema in
+    if is_normal sema exp then Normal
+    else if is_nan sema exp fra then Nan
+    else if is_inf sema exp fra then Inf
+    else if is_subnormal exp fra  then Subnormal
+    else Zero
+
+end
+
 
 module Try = struct
 
@@ -68,7 +99,7 @@ module Try = struct
 
   type my_float = {
     sign : bool;
-    exp  : int; (* biased exp  *)
+    exp  : int; (* biased exp, i.e. E = e + bias  *)
     fra  : word;
     sema : sema;
   }
@@ -98,54 +129,24 @@ module Try = struct
   let shift_fra_left t bits =
     {t with exp = t.exp - bits; fra = lsl_fra t.fra bits}
 
-  let estimate_gone t bits =
-    match lsb t with
-    | None -> `Zero
-    | Some lsb ->
-      if bits <= lsb then `Zero
-      else if bits = lsb + 1 then `Half
-      else if bits <= Sema.precision t.sema
-           && Word.is_one (Word.extract_exn
-                             ~hi:(bits - 1) ~lo:(bits-1) t.fra) then
-        `MoreThenHalf
-      else `LessThenHalf
-
-  let str_of_gone = function
-    | `Zero -> "zero"
-    | `Half -> "half"
-    | `MoreThenHalf -> "more then half"
-    | `LessThenHalf -> "less then half"
-
-
   let shift_fra_right t bits =
     let gone = Word.extract_exn ~hi:(bits - 1) t.fra  in
     {t with exp = t.exp + bits; fra = lsr_fra t.fra bits}, gone
 
   let exp_bits s = Sema.total s - Sema.precision s
-  let unbiased_exp t = t.exp - Sema.bias t.sema
 
-  let is_normal t =
-    let e = unbiased_exp t in
-    1 - Sema.bias t.sema <= e && e <= 2 * Sema.max_exponent t.sema
+  let fraction t = Word.extract ~hi:(Word.bitwidth t.fra - 2) t.fra
 
-  let is_nan t =
-    let e = unbiased_exp t in
-    e = 2 * Sema.max_exponent t.sema + 1 && not (Word.is_zero t.fra)
-
-  let is_inf t =
-    let e = unbiased_exp t in
-    e = 2 * Sema.max_exponent t.sema + 1 && Word.is_zero t.fra
-
-  let is_subnormal t = t.exp = 0 && not (Word.is_zero t.fra)
-
-  let is_zero t = t.exp = 0 && Word.is_zero t.fra
+  (* Add a category? for subnormal *)
+  let cat t = Category.create t.sema t.exp t.fra
 
   (* TODO: a rounding propably should be used here  *)
   (* TODO: result should reflect what exactly we got: Norm, Inf, Nan *)
+  (* TODO: normalize should affect a leading significant bit *)
   let normalize t =
-    if is_normal t then t
-    else
-      match msb t with
+    match cat t with
+    | Normal -> t
+    | _ -> match msb t with
       | None -> failwith "will see"
       | Some msb ->
         let change = msb - Sema.precision t.sema in
@@ -162,36 +163,60 @@ module Try = struct
             fst @@ shift_fra_right t change
           else t
 
+  let check_length sema bits =
+    let bitwidth = Word.bitwidth bits in
+    let total = Sema.total sema in
+    if bitwidth < total then
+      failwith
+        (sprintf "Failed to create float: %d bits expected, %d got"
+           total bitwidth)
+
+  let sign_exn sema bits =
+    let sign_bit = Sema.total sema - 1 in
+    Word.extract_exn ~hi:sign_bit ~lo:sign_bit bits |> Word.is_one
+
   let exp_hi sema = Sema.total sema - 2
   let exp_lo sema = Sema.precision sema - 1
 
+  let exp_exn sema bits =
+    Word.extract_exn ~hi:(exp_hi sema) ~lo:(exp_lo sema) bits |>
+    Word.to_int_exn
+
+  let fra_exn sema bits =
+    if Sema.integer_bit sema then
+      Word.extract_exn ~hi:(Sema.precision sema - 1) bits
+    else
+      let exp = exp_exn sema bits in
+      let fra = Word.extract_exn ~hi:(Sema.precision sema - 2) bits in
+      let leading_bit = match Category.create sema exp fra with
+        | Normal -> Word.b1
+        | _ -> Word.b0 in
+      Word.concat leading_bit fra
+
+  (** [of_bits sema bits] - create only from normal bits *)
   let of_bits sema bits =
-    let bits =
-      if Word.bitwidth bits < Sema.total sema then
-        let x = Word.zero (Sema.total sema - Word.bitwidth bits) in
-        Word.concat x bits
-      else bits in
-    let sign_bit = Sema.total sema - 1 in
-    let sign = Word.extract_exn ~hi:sign_bit ~lo:sign_bit bits
-               |> Word.is_one in
-    let exp = Word.extract_exn ~hi:(exp_hi sema) ~lo:(exp_lo sema) bits
-              |> Word.to_int_exn in
-    let fra = Word.extract_exn ~hi:(exp_lo sema - 1) bits in
+    check_length sema bits;
+    let sign = sign_exn sema bits in
+    let exp = exp_exn sema bits in
+    let fra = fra_exn sema bits in
     normalize {sign; exp; fra; sema}
 
   let to_bitstring t =
     let exp = t.exp in
     let sign = if t.sign then "1" else "0" in
     let exp = Word.of_int ~width:(exp_hi t.sema - exp_lo t.sema + 1) exp in
-    sprintf "%s%s%s" sign (string_of_bits exp) (string_of_bits t.fra)
+    let fra = Word.extract_exn ~hi:(exp_lo t.sema - 1) t.fra in
+    sprintf "%s%s%s" sign (string_of_bits exp) (string_of_bits fra)
 
+  (* TODO: check an integer bit here and add it if needed  *)
   let bits t =
     let sign = if t.sign then Word.b1 else Word.b0 in
     let exp = Word.of_int ~width:(exp_bits t.sema) t.exp in
-    Word.concat (Word.concat sign exp) t.fra
+    let fra = Word.extract_exn ~hi:(exp_lo t.sema - 1) t.fra in
+    Word.concat (Word.concat sign exp) fra
 
   let to_string_temp t =
-    let exp = unbiased_exp t in
+    let exp = t.exp - Sema.bias t.sema in
     let base = 2.0 ** float exp in
     let bits = enum_bits t.fra in
     let _,m = Seq.fold bits ~init:(1.0,0.0) ~f:(fun (d,r) b ->
@@ -201,6 +226,43 @@ module Try = struct
     let sign = if t.sign then "-" else "" in
     sprintf "%s%f" sign ((1.0 +. m) *. base )
 
+  let cmp f x y =
+    let r = f x y in
+    if Int.(r > 0) then GreaterThan
+    else if Int.(r < 0) then LessThan
+    else Equal
+
+  let cmp_abs x y =
+    match cmp Int.compare x.exp y.exp with
+    | Equal -> cmp Word.compare x.fra y.fra
+    | x -> x
+
+  let is_pos t = not t.sign
+
+  let cmp_of_sign x y =
+    if x.sign = y.sign then Equal
+    else if is_pos x then GreaterThan
+    else LessThan
+
+  (* TODO: what to do with the subnormal numbers  *)
+  let cmp x y =
+    match cat x, cat y with
+    | Nan, _ | _, Nan -> Unordered
+    | Inf, Normal | Inf, Zero | Normal, Zero ->
+      if is_pos x then GreaterThan else LessThan
+    | Normal, Inf | Zero, Inf | Zero, Normal ->
+      if is_pos y then LessThan else GreaterThan
+    | Inf, Inf -> cmp_of_sign x y
+    | Zero, Zero -> Equal
+    | Normal, Normal ->
+      if x.sign <> y.sign then cmp_of_sign x y
+      else
+        let r = cmp_abs x y in
+        if is_pos x then r
+        else match r with
+          | GreaterThan -> LessThan
+          | LessThan -> GreaterThan
+          | x -> x
 
   let add x y =
     let x',y', gone =
@@ -212,12 +274,8 @@ module Try = struct
         let x', gone = shift_fra_right x dif in
         x', y, gone in
     let _gone' = Word.extract_exn ~hi:(Word.bitwidth x.fra - 1) gone in
-    let _t = {x with fra=_gone'} in
-    printf "gone is %s\n" (to_bitstring _t);
-    printf "shift:\n %s\n %s\n" (to_bitstring x) (to_bitstring x');
     let fra = Word.(x'.fra + y'.fra) in
     normalize {x with exp = x'.exp ; fra}
-
 
 end
 
@@ -229,39 +287,13 @@ let double_of_float f =
   let w = Word.of_int64 ~width:64 (Int64.bits_of_float f) in
   Try.of_bits Sema.ieee_double w
 
+let float_of_single x =
+  Int32.float_of_bits (Word.to_int32_exn (Try.bits x))
+
+let float_of_double x =
+  Int64.float_of_bits (Word.to_int64_exn (Try.bits x))
+
 module Test_space = struct
-
-  let exp  w = Word.extract_exn ~hi:30 ~lo:23 w |> Word.to_int_exn
-  let frac w = Word.extract_exn ~hi:22 w |> Word.to_int_exn
-
-  let inspect x =
-    let bits = Int32.bits_of_float x in
-    let w = Word.of_int32 ~width:32 bits in
-    let bits = Word.enum_bits w BigEndian in
-    let (@@) = sprintf "%s%d" in
-    let str = Seq.foldi bits ~init:"" ~f:(fun i s x ->
-        let s = match i with
-          | 1 | 9 -> sprintf "%s_" s
-          | _ -> s in
-        if x then s @@ 1
-        else s @@ 0) in
-    str, exp w, frac w
-
-  let temp () =
-    let w = Word.of_int ~width:23 420000 in
-    let e = Word.of_int ~width:8 129 in
-    let x = Word.concat Word.b0 e in
-    let x = Word.concat x w in
-    let x = Word.to_int32_exn x in
-    let x = Int32.float_of_bits x in
-    printf "temp: %g\n" x
-
-  let a () =
-    let test x =
-      let bits,exp,frac = inspect x in
-      printf "%-5g: %s (exp: %d, frac: %d)\n" x bits exp frac in
-    let xs = [5.9; 4.2; 0.42] in
-    List.iter ~f:test xs
 
   let word_of_float x =
     let x = Int32.bits_of_float x in
@@ -269,16 +301,11 @@ module Test_space = struct
 
   let test1 =
     let z = 4.2 in
-    let q = 0.2 in
-    let x = word_of_float (q +. z) in
-    let str1 = string_of_bits x in
-    let b1 = single_of_float q in
-    let b2 = single_of_float z in
+    let q = 2.78 in
+    let b1 = double_of_float q in
+    let b2 = double_of_float z in
     let b = Try.add b1 b2 in
-    let x = Int32.float_of_bits (Word.to_int32_exn (Try.bits b)) in
-    printf "0.2: %s\n" (Try.to_bitstring b1);
-    printf "we got %g\n" x;
-    printf "test:\n should: %s\n got:    %s\n"
-      str1 (Try.to_bitstring b)
+    let x = float_of_double b in
+    printf "%g + %g = %g\n" z q x
 
 end
